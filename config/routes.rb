65af9a9d9592fc6a3d0b15d23d6763cb9149c8ff
David Chiu <davidwchiu@gmail.com>
2019-09-07 21:55:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   get 'authors/:id/posts'
   get 'authors/:id/posts/:post_id'
-
+  get 'authors/:id/posts', to: 'authors#posts_index'
+  get 'authors/:id/posts/:post_id', to: 'authors#post'
+  
 end
